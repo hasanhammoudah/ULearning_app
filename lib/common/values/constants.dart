38c9dc878017ee99bbfe3e8b50ddb3d7048f13bb
hasan <hassun.h98@gmail.com>
2024-03-14 22:29:47 +0300

    NEW UPDATE

--- lib/common/values/constants.dart
+++ lib/common/values/constants.dart
@@ -1,5 +1,5 @@
 class AppConstants {
-  static const String SERVER_API_URL = "https://0492-2a01-9700-158e-b500-fc2b-6c5b-f1db-f800.ngrok-free.app/";
+  static const String SERVER_API_URL = "https://4a1c-92-253-29-81.ngrok-free.app/";
   static const String SERVER_UPLOADS = "${AppConstants.SERVER_API_URL}uploads/";
   static const String STORAGE_DEVICE_OPEN_FIRST_TIME = 'device_first_open';
   static const String STORAGE_USER_PROFILE_KEY = 'user_profile_key';
